3fde96a309424024f2496de9b2c15ee3dc4b0153
kathirr007 <kathirr007@gmail.com>
2017-01-15 02:04:57 +0530

    travis updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -16,26 +16,20 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 # git status
 
 
-setup_git() {
+
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-}
 
-commit_website_files() {
   git checkout -b gh-pages
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git status
-}
 
-upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages gh-pages
-}
 
-setup_git
-commit_website_files
-upload_files
+
+
 
 
 
