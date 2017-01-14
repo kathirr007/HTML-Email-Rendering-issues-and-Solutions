c49b88305e896b213b37cffde1e5db3bc0fdd4dc
kathirr007 <kathirr007@gmail.com>
2017-01-15 01:51:03 +0530

    travis updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,21 +1,41 @@
-#!/bin/bash
+#!/bin/sh
 
 GH_REPO="github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
-git config user.email "kathirr007@gmail.com"
-git config user.name "kathirr007"
+# git config --global user.email "travis@travis-ci.org"
+# git config --global user.name "Travis CI"
+
+# git status
+# git add .
+# git status
+# git commit -m "deployed to github pages"
+# git remote add origin-pages https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git > /dev/null 2>&1
+# git push --force --quiet https://${GH_TOKEN}@${GH_REPO} master
+# git status
 
 
-git status
-git add .
-git status
-git commit -m "deployed to github pages"
-git remote add origin-pages https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git > /dev/null 2>&1
-git push --force --quiet https://${GH_TOKEN}@${GH_REPO} master
-git status
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
+commit_website_files() {
+  git checkout -b gh-pages
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git status
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@{GH_REPO} > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages
+}
+
+setup_git
+commit_website_files
+upload_files
 
 
 
