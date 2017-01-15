d5429891d3f140df9976a4a3b47937f11671d37b
kathirr007 <kathirr007@gmail.com>
2017-01-15 15:05:06 +0530

    deploy script updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,6 +5,25 @@ GH_REPO="github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git"
 
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git out
+cd out
+cd builds/development
+git init
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+git status
+git add .
+git status
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git status
+git push -ufq https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git master:gh-pages > /dev/null 2>&1
+
 # git config --global user.email "travis@travis-ci.org"
 # git config --global user.name "Travis CI"
 
@@ -17,19 +36,7 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 # git status
 
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
-
-git push origin --delete https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git :gh-pages
-git checkout -b gh-pages
-git checkout --orphan gh-pages
-git rm -rf .
-git add .
-git commit -m "Init gh-pages"
-git push --set-upstream https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git origin:gh-pages
-git checkout master
-# gulp travis
-gulp deploy
+# gulp deploy
 # git status
 # git add "./builds/development/**/*"
 # git status
