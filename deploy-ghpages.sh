533bd0e7b240d673623c83c3fb0be1058f24ab47
kathirr007 <kathirr007@gmail.com>
2017-01-15 11:07:00 +0530

    travis and deploy updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -25,7 +25,7 @@ git add "./builds/development/**/*"
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git status
 
-# git remote add origin-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null 2>&1
+git remote add origin-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null 2>&1
 git push --force --quiet --set-upstream origin-pages gh-pages
 
 
