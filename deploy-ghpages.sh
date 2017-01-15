c5527ce30e4c9619c93bb77bfb0b9e538a5599cb
kathirr007 <kathirr007@gmail.com>
2017-01-15 15:16:26 +0530

    deploy script updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -12,32 +12,23 @@ TARGET_BRANCH="gh-pages"
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 # git clone https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git out
+gulp travis
 # cd out
-# cd builds/development
-# git init
-# git config --global user.email "travis@travis-ci.org"
-# git config --global user.name "Travis"
-# git status
-# git add .
-# git status
-# git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-# git status
-# git push -ufq https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git master:gh-pages > /dev/null 2>&1
-
-# git config --global user.email "travis@travis-ci.org"
-# git config --global user.name "Travis CI"
-
-# git status
-# git add .
-# git status
-# git commit -m "deployed to github pages"
-# git remote add origin-pages https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git > /dev/null 2>&1
-# git push --force --quiet https://${GH_TOKEN}@${GH_REPO} master
-# git status
-
+cd builds/development
+git init
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
-gulp deploy
+git status
+git add .
+git status
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git status
+git push -ufq https://${GH_TOKEN}@github.com/kathirr007/HTML-Email-Rendering-issues-and-Solutions.git master:gh-pages > /dev/null 2>&1
+
+
+# git config --global user.email "travis@travis-ci.org"
+# git config --global user.name "Travis"
+# gulp deploy
 
 # git status
 # git add "./builds/development/**/*"
