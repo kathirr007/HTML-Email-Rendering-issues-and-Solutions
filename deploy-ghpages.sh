71917ad4419a0522d07e657cb11e0853fd347a2c
kathirr007 <kathirr007@gmail.com>
2017-01-15 13:34:42 +0530

    deploy script updated

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -20,7 +20,7 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-git push origin -f --delete gh-pages
+git push origin --delete gh-pages
 git checkout -b gh-pages
 gulp travis
 git status
