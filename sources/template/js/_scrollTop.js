75f8da9c620284e3286fc69fdb72ae891d140cab
kathirr007 <kathirr007@gmail.com>
2017-01-16 16:05:42 +0530

    Fixed scroll top

--- sources/template/js/_scrollTop.js
+++ sources/template/js/_scrollTop.js
@@ -8,6 +8,27 @@ $(window).scroll(function() {
 $('a[href="#top"]').click(function() {
     $("html, body").animate({
         scrollTop: 0
-    }, fast, easein);
+    }, 143);
     return false;
 });
+
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 100) {
+//         $('.scrollup').fadeIn();
+//     } else {
+//         $('.scrollup').fadeOut();
+//     }
+// });
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//             $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 500);
+//             return false;
+//         }
+//     }
+// });
