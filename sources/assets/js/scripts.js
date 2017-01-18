139e97db1da22d3e927a33478acb6dd0f9d2cfed
kathirr007 <kathirr007@gmail.com>
2017-01-19 00:15:39 +0530

    Solutions updated

--- sources/assets/js/scripts.js
+++ sources/assets/js/scripts.js
@@ -1,5 +1,5 @@
 /*jshint multistr: true */
-$(document).ready (function() {
+$(document).ready(function() {
     var xmlhttp = new XMLHttpRequest()
       , url = "assets/json/cart.json"
       , url2 = "assets/json/issues_n_solutions.json"
@@ -35,7 +35,7 @@ $(document).ready (function() {
         var issues_n_solutions = JSON.parse(response), issuesNSolutionsList = issues_n_solutions[0].issuesAndSolutions, //             pfizerSolutions = issuesNSolutionsList[0].pfizer,
         //             paypalSolutions = issuesNSolutionsList[0].paypal,
         //             goaheadSolutions = issuesNSolutionsList[0].go_ahead,
-        i, solutionsList = "", subTotal, discount, estimatedTotal, shipping, shippingPrice = "", subTotalPrice = "", discountPrice = "", estimatedTotalPrice = "", pricearray = [], quantityarray = [];
+        i, solutionsList = "", pricearray = [], quantityarray = [];
         console.log(issues_n_solutions);
         console.log(issuesNSolutionsList);
         // console.log(pfizerSolutions);
@@ -45,12 +45,15 @@ $(document).ready (function() {
             var issueId = issuesNSolutionsList[idx].issue_id
               , issueName = issuesNSolutionsList[idx].issue_name
               , solutions = issuesNSolutionsList[idx].solutions
-              , codepenUrl = issuesNSolutionsList[idx].codepen_url
-              , codepenUrl = codepenUrl ? '<a href="' + codepenUrl + '" target="_blank">here</a>' : '<strong>Not Available</strong>'
               , sanSolutions = replaceHtmlEntites;
-            // console.log(solutions);
+            console.log(solutions);
             // console.log(typeof(solutions));
-            // console.log(codepenUrl);
+            for(i=0; i<solutions.length; i++){
+                var codepenUrl = solutions[i].codepen_url;
+                codepenUrl = codepenUrl ? '<a href="' + codepenUrl + '" target="_blank">here</a>' : '<strong>Not Available</strong>';
+            }
+            console.log(codepenUrl);
+
             solutionsList += '<div class="panel panel-default">\
                         <a data-toggle="collapse" data-parent="#dynamic_datas" href="#collapse' + issueId + '">\
                             <div class="panel-heading">\
@@ -60,9 +63,11 @@ $(document).ready (function() {
                         <div id="collapse' + issueId + '" class="panel-collapse collapse">\
                             <div class="panel-body">';
             $.each(solutions, function(item) {
+                var codepenUrl = solutions[item].codepen_url;
+                    codepenUrl = codepenUrl ? '<a href="' + codepenUrl + '" target="_blank">here</a>' : '<strong>Not Available</strong>';
                 solutionsList += '<h3 class="text-center">Solution ' + solutions[item].id + '</h3>\
                 <div>' + replaceHtmlEntites(solutions[item].solution) + '</div>\
-                <p class="codepen-url">Codepen Url: ' + codepenUrl + '</p>';
+                <p class="codepen-url">To checkout more in detail please check code in Codepen: ' + codepenUrl + '</p>';
             });
             //             for(i=0; i<solutions.length; i++){
             //                 solutionsList += '<p>'+solutions[i].solution+'</p>';
@@ -70,15 +75,12 @@ $(document).ready (function() {
             solutionsList += '</div></div></div>';
         });
         document.getElementById('dynamic_datas').innerHTML = solutionsList;
-        
     }
-
-// @include ../../template/js/_scrollTop.js
-//  ../../template/js/_scrollmagic.js
-   
-
-
+    // @include ../../template/js/_scrollTop.js
+    //  ../../template/js/_scrollmagic.js
+    function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
 
 });
-
-
