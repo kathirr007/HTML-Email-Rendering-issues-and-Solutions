d88b3f641d36171ee5e01fe7dc6b56a082bf62cf
kathirr007 <kathirr007@gmail.com>
2017-01-24 04:09:26 +0530

    script updated for search highlighting

--- builds/development/assets/js/scripts.js
+++ builds/development/assets/js/scripts.js
@@ -120,6 +120,39 @@ $(document).ready(function() {
         });
         document.getElementById('dynamic_datas').innerHTML = solutionsList;
     }
-    // @include ../../template/js/_scrollTop.js
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+    $('a[href="#top"]').click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 143);
+        return false;
+    });
+    
+    
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.scrollup').fadeIn();
+    //     } else {
+    //         $('.scrollup').fadeOut();
+    //     }
+    // });
+    // $('a[href*="#"]:not([href="#"])').click(function() {
+    //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //         if (target.length) {
+    //             $('html, body').animate({
+    //                 scrollTop: target.offset().top
+    //             }, 500);
+    //             return false;
+    //         }
+    //     }
+    // });
     //  ../../template/js/_scrollmagic.js
 });
